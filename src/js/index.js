df514fc4eedc3c1b70750636d343edff4b018768
Ricardo <ricardo.23.03.1749@gmail.com>
2024-07-30 22:35:08 -0300

    primeiro commit

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+const listaSelecaoPokemons = document.querySelectorAll(".pokemon");
+console.log(listaSelecaoPokemons);
+const pokemonsCard = document.querySelectorAll(".cartao-pokemon");
+
+listaSelecaoPokemons.forEach((pokemon) => {
+  pokemon.addEventListener("click", () => {
+    console.log(pokemon);
+
+    const cartaoPokemonAberto = document.querySelector(".aberto");
+    cartaoPokemonAberto.classList.remove("aberto");
+
+    const idPokemonSelecionado = pokemon.attributes.id.value;
+
+    const idDoCartaoPokemonParaAbrir = "cartao-" + idPokemonSelecionado;
+    const cartaoPokemonParaAbrir = document.getElementById(
+      idDoCartaoPokemonParaAbrir
+    );
+    cartaoPokemonParaAbrir.classList.add("aberto");
+
+    const pokemonAtivoNaListagem = document.querySelector(".ativo");
+    pokemonAtivoNaListagem.classList.remove("ativo");
+
+    const pokemonSelecionadoNaListagem =
+      document.getElementById(idPokemonSelecionado);
+    pokemonSelecionadoNaListagem.classList.add("ativo");
+  });
+});
